898aad07597e4406b35d5db9739dcb04dc958f73
Caren <carenjepchumba4@gmail.com>
2025-06-01 12:42:28 +0300

    Added js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const contact = document.querySelector("section:last-of-type");
+  contact.addEventListener("click", () => {
+    alert("Feel free to reach out to me!");
+  });
+});
